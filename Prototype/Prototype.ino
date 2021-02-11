5cb9199e83ecece317020846d9c1a55a0c75ec44
whyonly8 <52013911+whyonly8@users.noreply.github.com>
2021-02-11 07:32:26 -0700

    Adding Distance and Formatting Comments

--- Prototype/Prototype.ino
+++ Prototype/Prototype.ino
@@ -1,32 +1,36 @@
-/*
-  Code By: Deagan Malloy - Code for the Arduino MEGA - subject to change to PPM for the remote instead of PWM
-  For: Senior design, will be basic code for how to interface with the remote control for the toy car
-  
-  PINS: 2: Throttle input
-        3: Steer Input
-        4: Headlights switch input
-        5: Neutral/ Drive/ Reverse switch input
+/*  Code For E-GoKart Senior Design Project for Colorado State University
+ *  Written By: Deagan Malloy
+ *  Written For: Arduino Mega
+ *  This will be the code which takes the PPM signal from the remote and converts it to PWM for the Motor Driver.
+ *  It will also take in distance measurements from LiDar modules and factors that distance into the drive output.
+ *  The requirements are two UART Tx and Rx ports, 4 digital PWM input pins, 3 digital PWM output pins, and 1 analog output pin.
+ *  
+ *  PINS:  2: Throttle input
+ *         3: Steer Input
+ *         4: Headlights switch input
+ *         5: Neutral/ Drive/ Reverse switch input
+ *         9: Output for the Steering Servo
+ *        10: PWM output for 1st input to H-Bridge
+ *        11: PWM output for 2nd input to H-Bridge
+ *        22: Output to turn on Headlights 
+ */
 
-        9 : Output for the Steering Servo
-        10: PWM output for 1st input to H-Bridge
-        11: PWM output for 2nd input to H-Bridge
-        22: Output to turn on Headlights (TBD)
-*/
-
-#include<Servo.h>
+#include <Servo.h>
 Servo steeringServo;
-const int servoPIN = 9;       //pin for servo output
 
-// setting the four inputs from the reciever to PWM in ports
-const int throttle = 2;       // channel 3 on reciever
-const int steer = 3;          // channel 4 on reciever
-const int DriveOrReverse = 5; // channel 6 on reciever
-const int headlights = 4;     // channel 5 on reciever
+#include <TFMini.h>
+TFMini Sensor1;
+TFMini Sensor2;
 
-const int PWMout1 = 10;       // these two will be the outputs for the motors
-const int PWMout2 = 11;       // left and right motor will run at same speed by only using 2 outputs
-     
-const int lightsPIN = 22;     // pin used to turn the headlights on and off
+const int throttle = 2;           // channel 3 on reciever
+const int steer = 3;              // channel 4 on reciever
+const int DriveOrReverse = 5;     // channel 6 on reciever
+const int headlights = 4;         // channel 5 on reciever
+
+const int servoPIN = 9;           // Pin for servo output
+const int PWMout1 = 10;           // These two will be the outputs for the motors
+const int PWMout2 = 11;           // Left and right motor will run at same speed by only using 2 outputs
+const int lightsPIN = 22;         // Pin used to turn the headlights on and off
 
 int servoPosNew = 90;
 
@@ -42,17 +46,27 @@ void setup() {
 
   steeringServo.attach(servoPIN);
 
-  Serial.begin(9600); // starts serial with baud rate of 9600
-  delay(50);
+  Serial.begin(9600);                                          // Serial with baud rate of 9600
+  Serial1.begin(115200);
+  Serial2.begin(115200);                                       // Serials for the Sensors
+  
+  Sensor1.begin(&Serial1);    
+  Sensor2.begin(&Serial2);                                     // Initialize Sensors with Serial
+
+  Sensor1.setSingleScanMode();
+  Sensor2.setSingleScanMode();                                 // Force each sensor to only read when triggered
+  
+  delay(50);                                                   // Let signals stabilize
 }
 
 void loop() {
-  static int throttle_pulse = 1000; // These 4 are the pulse length in microseconds that they are active high
-  static int steer_pulse = 1500;
-  static int drive_state_pulse = 1000;
-  static int headlights_pulse = 1000;
-  static int drive_state = 0; // 0=neutral, 1=drive, 2=reverse
-  static int channel_in = 0; // 0=throttle, 1=steer, 2=drive_state, 3=headlights - counter for getting pulse inputs
+  static int throttle_pulse = 1000;                            // These 4 are the pulse length in microseconds that they are active high
+  static int steer_pulse = 1500;                               // These 4 are the pulse length in microseconds that they are active high
+  static int drive_state_pulse = 1000;                         // These 4 are the pulse length in microseconds that they are active high
+  static int headlights_pulse = 1000;                          // These 4 are the pulse length in microseconds that they are active high
+  static int drive_state = 0;                                  // 0=neutral, 1=drive, 2=reverse
+  static bool clear_path = true;                               // Whether or not the path is clear
+  //static int channel_in = 0; // 0=throttle, 1=steer, 2=drive_state, 3=headlights - counter for getting pulse inputs
   
 // this will be moved to interupt section that runs every 20ms (how long the period is of PWM signals to DC motor
   /*if(channel_in == 0){ // update throttle
@@ -76,6 +90,8 @@ void loop() {
     channel_in = 0;
   }*/
 
+  clear_path=distanceCheck();                                  // Use LiDar to see if there is an obstacle
+  
   throttle_pulse = pulseIn(throttle, HIGH, 30000);
   updateThrottleOut(throttle_pulse, drive_state);
 
@@ -88,74 +104,89 @@ void loop() {
   headlights_pulse = pulseIn(headlights, HIGH, 30000);
   updateHeadlights(headlights_pulse);
   
-
 }
 
 void updateThrottleOut(int &pulse, int &drive_state){
-  static int setRpm1 = 0, setRpm2 = 0;
+  static int setRpm1 = 0, setRpm2 = 0;            // Initialize RPM's
 
-  if(drive_state == 0){ // We are in Neutral - Ouput L L to start breaking and bring car to stop
+  if(drive_state == 0){                           // Neutral - Ouput Low Low to start breaking and bring car to stop
     setRpm1 = 0;
     setRpm2 = 0;
-    analogWrite(PWMout1, setRpm1);  // set them both to 0
-    analogWrite(PWMout2, setRpm2);  // set them both to 0
+    analogWrite(PWMout1, setRpm1);                // Set both PWM to 0
+    analogWrite(PWMout2, setRpm2);                // Set both PWM to 0
     
-  } else if(drive_state == 1){                // We are in Drive - Output H L to drive forward
-    if(pulse > 1100 && pulse < 2050)
-      setRpm1 = map(pulse, 1100, 2050, 0, 255); // map input pulse to PWM output duty cycle between 0 and 255
-    else 
-      setRpm1 = 0;                              // if input pulse is too slow, stay still
-    setRpm2 = 0;                                // Second needs to be low to drive Forwards
-    analogWrite(PWMout2, setRpm2);  // set them both to 0
-    delay(10);                      // small delay
-    analogWrite(PWMout1, setRpm1);  // set PWM1 to PWM signal
+  } else if(drive_state == 1 && clear_path){      // Drive - Output High, Low to drive forward
+      if(pulse > 1100 && pulse < 2050)
+        setRpm1 = map(pulse, 1100, 2050, 0, 255); // Map input pulse to PWM output duty cycle between 0 and 255
+      else 
+        setRpm1 = 0;                              // If input pulse is too slow, stay still
+        
+      setRpm2 = 0;                                // Second needs to be Low to drive Forwards
+      analogWrite(PWMout2, setRpm2);              // Set second PWM Low
+      delay(10);                                  // Small delay
+      analogWrite(PWMout1, setRpm1);              // Set first PWM to PWM signal
     
-  } else if(drive_state == 2){                // We are in Reverse - Output L H to drive backwards (just opposite of drive)
-   if(pulse > 1100 && pulse < 2050)
-      setRpm2 = map(pulse, 1100, 2050, 0, 255); // map input pulse to PWM output duty cycle between 0 and 255
-    else 
-      setRpm2 = 0;                              // if input pulse is too slow, stay still
-    setRpm1 = 0;                                // First needs to be low to Reverse
-    analogWrite(PWMout1, setRpm1);  // set them both to 0
-    delay(10);                      // small delay
-    analogWrite(PWMout2, setRpm2);  // set PWM2 to PWM signal
+  } else if(drive_state == 2){                    // Reverse - Output Low High to drive backwards (just opposite of drive)
+      if(pulse > 1100 && pulse < 2050)
+        setRpm2 = map(pulse, 1100, 2050, 0, 255); // map input pulse to PWM output duty cycle between 0 and 255
+      else 
+        setRpm2 = 0;                              // If input pulse is too slow, stay still
+        
+      setRpm1 = 0;                                // First needs to be Low to Reverse
+      analogWrite(PWMout1, setRpm1);              // Set first PWM Low
+      delay(10);                                  // Small delay
+      analogWrite(PWMout2, setRpm2);              // Set second PWM to PWM signal
     
-  } else {  // should never happen, but if something goes wrong, do nothing by outputting both Low
-    setRpm1 = 0;
-    setRpm2 = 0;
-    analogWrite(PWMout1, setRpm1);  // set them both to 0
-    analogWrite(PWMout2, setRpm2);  // set them both to 0
+  } else {                                        // Default Case - Output Low Low to stop
+      setRpm1 = 0;
+      setRpm2 = 0;
+      analogWrite(PWMout1, setRpm1);              // Set them both to 0
+      analogWrite(PWMout2, setRpm2);              // Set them both to 0
   }
 }
 
-void updateHeadlights(int &pulse){ // Reads in if pulse is long or short, and turns on and off headlights
-    if(pulse > 1500 && pulse < 2050){
-      // turn on headlights - just output constant 5V output
-      digitalWrite(lightsPIN, HIGH);
-    } else {
-      // turn off headlights - turn off that output
-      digitalWrite(lightsPIN, LOW);
-    }
+void updateHeadlights(int &pulse){                // Reads in if pulse is long or short, and turns on and off headlights
+    if(pulse > 1500 && pulse < 2050)
+      digitalWrite(lightsPIN, HIGH);              // Turn on headlights
+     else 
+      digitalWrite(lightsPIN, LOW);               // Turn off headlights    
 }
 
 int updateDriveState(int &pulse){
-  if(pulse > 1600 && pulse < 2050){ // It is in Reverse
+  if(pulse > 1600 && pulse < 2050){               // Reverse
     return 2;
-  } else if(pulse > 1400 && pulse <= 1600){ // It is in Drive
-    return 1;
-  } else if(pulse > 950 && pulse <= 1400){ // It is in Neutral
-    return 0;
+  } else if(pulse > 1400 && pulse <= 1600){       // Drive
+      return 1;
+  } else if(pulse > 950 && pulse <= 1400){        // Neutral
+      return 0;
   }
 }
 
 void updateSteering(int &pulse){
-  if(pulse < 950 || pulse > 2050){  // if invalid pulse length, return to original position 
+  if(pulse < 950 || pulse > 2050){                // If invalid pulse length, return to original position 
     steeringServo.write(90);  
     return;
   } 
-  servoPosNew = map(pulse, 980, 2020, 0, 180);// + 12;  // Will need to change this to only rotate 90 degrees // with of offset of 12 currently in order to recenter after turning 
+  servoPosNew = map(pulse, 980, 2020, 0, 180);    // + 12. Will need to change this to only rotate 90 degrees with of offset of 12 currently in order to recenter after turning 
   if(servoPosNew > 180)
     servoPosNew = 180;
-  steeringServo.write(servoPosNew);
+    
+  steeringServo.write(servoPosNew);               // Write the new position
   delay(50);
+}
+
+boolean distanceCheck(){
+  static uint16_t distance_one = 0; 
+  static uint16_t distance_two = 0;               // Initialize the Variables
+  Sensor1.externalTrigger();                      // Trigger the sensor
+  distance_one = Sensor1.getDistance();           // Read Data
+  Sensor2.externalTrigger();                      // Repeat
+  distance_two = Sensor2.getDistance();
+  
+  if (distance_one <= 100 || distance_two <= 100) // Ensure that the distance is correct
+    return false;                                 // Less than 100 cm is unsafe
+  else
+    return true;                                  // Greater than 100 cm is safe
+                                                  // Measurements which are out of range will give the value 65535, which is the maximum value of a 16 bit unsigned integer.
+                                                  // The range of this sensor should be 1500 cm, but has not been verified.
 }
