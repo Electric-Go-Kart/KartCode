a06a97255b70981403fcfba472fa5ea0fc0c69b2
whyonly8 <52013911+whyonly8@users.noreply.github.com>
2021-02-17 12:20:11 -0700

    Update PressureModule.ino
    Changed offset values and ranges to fit with the actual measurements, instead of default ones.

--- Modules/PressureModule/PressureModule.ino
+++ Modules/PressureModule/PressureModule.ino
@@ -14,9 +14,9 @@ void setup() {
 void loop() {
   static float pressure = 0;
   pressure = analogRead(pressurePIN);
-  pressure = (pressure-102.4)/819.2*500;
-  // 0.5 is the smallest voltage which corresponds to the ADC value 102.4, 4.5 is the largest voltage which corresponds to the ADC value 921.6, 819.2 is the range of ADC values
-  // The ADC measurement is offset from the minimum allowable voltage, divided by the range of allowed values, and multiplied by the measureable range of pressure
+  pressure = (pressure-98)/828*500;
+  // The ADC value 98 is the lowest measurement point, the ADC value 926 is the maximum measurement, 828 is the range of ADC values
+  // The ADC measurement is offset from the minimum measurement, divided by the range of allowed values, and multiplied by the measureable range of pressure
   Serial.print(pressure, 1);                // Formated with one decimal point
   Serial.println(" psi");
   delay(250);                               //  Delay in milliseconds between read values
