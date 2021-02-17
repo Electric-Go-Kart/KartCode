9863f95495d45fe0fffe928f4bbe5f2233bbf82f
Deagan Malloy <dmalloy@rams.colostate.edu>
2021-02-17 16:05:29 -0700

    updated code for braking with lidar

--- Prototype/Prototype.ino
+++ Prototype/Prototype.ino
@@ -1,7 +1,7 @@
 /*  Code For E-GoKart Senior Design Project for Colorado State University
  *  Written By: Deagan Malloy
  *  Written For: Arduino Mega
- *  This will be the code which takes the PPM signal from the remote and converts it to PWM for the Motor Driver.
+ *  This will be the code which takes the PWM signals from the remote and controls the motors, headlights, steering, and steering mode.
  *  It will also take in distance measurements from LiDar modules and factors that distance into the drive output.
  *  The requirements are two UART Tx and Rx ports, 4 digital PWM input pins, 3 digital PWM output pins, and 1 analog output pin.
  *  
@@ -24,15 +24,14 @@ TFMini Sensor2;
 
 const int throttle = 2;           // channel 3 on reciever
 const int steer = 3;              // channel 4 on reciever
-const int DriveOrReverse = 5;     // channel 6 on reciever
 const int headlights = 4;         // channel 5 on reciever
+const int DriveOrReverse = 5;     // channel 6 on reciever
+
 
 const int servoPIN = 9;           // Pin for servo output
 const int PWMout1 = 10;           // These two will be the outputs for the motors
 const int PWMout2 = 11;           // Left and right motor will run at same speed by only using 2 outputs
 const int lightsPIN = 22;         // Pin used to turn the headlights on and off
-
-int servoPosNew = 90;
 
 void setup() {
   pinMode(throttle, INPUT);
@@ -60,32 +59,15 @@ void setup() {
 }
 
 void loop() {
+  // only initialize the variables first time
   static int throttle_pulse = 1000;                            // These 4 are the pulse length in microseconds that they are active high
   static int steer_pulse = 1500;                               // These 4 are the pulse length in microseconds that they are active high
   static int drive_state_pulse = 1000;                         // These 4 are the pulse length in microseconds that they are active high
   static int headlights_pulse = 1000;                          // These 4 are the pulse length in microseconds that they are active high
   static int drive_state = 0;                                  // 0=neutral, 1=drive, 2=reverse
-<<<<<<< HEAD
-  //static int channel_in = 0; // 0=throttle, 1=steer, 2=drive_state, 3=headlights - counter for getting pulse inputs
-  
-// this will be moved to interupt section that runs every 20ms (how long the period is of PWM signals to DC motor
-  
-  throttle_pulse = pulseIn(throttle, HIGH, 30000);
-  updateThrottleOut(throttle_pulse, drive_state);
-
-  steer_pulse = pulseIn(steer, HIGH, 30000);
-  updateSteering(steer_pulse);
-
-  drive_state_pulse = pulseIn(DriveOrReverse, HIGH, 30000);
-  updateDriveState(drive_state_pulse);
-
-  headlights_pulse = pulseIn(headlights, HIGH, 30000);
-  updateHeadlights(headlights_pulse);
-=======
-  static bool safe_path = true;                               // Whether or not the path is clear
   static int counter = 0;
->>>>>>> 1483037100e5a205e05f27716bc832194e15a738
-  
+
+  // switch statement to control which function to update 
   switch(counter){
     case 0:
       throttle_pulse = pulseIn(throttle, HIGH, 30000);
@@ -110,6 +92,13 @@ void loop() {
   counter++;
 }
 
+/* takes in 2 integers as argumetns
+ * pulse is the pulse width in us from pin 2, or 3 on the reciever
+ * drive_state is the int 0, 1, or 2 for neutral, drive, or reverse
+ * complex function to determine the duty cycle to set to the motors
+ * uses analogWrite to output a PWM signal with different duty cycles to conrtol motor speeds
+ * returns void
+ */
 void updateThrottleOut(int &pulse, int &drive_state){
   static int setRpm1 = 0, setRpm2 = 0;            // Initialize RPM's
 
@@ -121,9 +110,11 @@ void updateThrottleOut(int &pulse, int &drive_state){
     
   } else if(drive_state == 1){                    // Drive - Output High, Low to drive forward
       int maxRPM = 0;
-      
+
+      Serial.print("Pulse: ");
+      Serial.println(pulse);
       if(pulse > 1100 && pulse < 2050) {      // Good input pulse 
-        int lidarDist = distanceCheck();        // distance to closest object (in cm)
+        int lidarDist = 500;//distanceCheck();        // distance to closest object (in cm)
         setRpm1 = map(pulse, 1100, 2050, 0, 255);
         
         if(lidarDist < 400){                    // if distance to object is too close -> set maxRPM
@@ -137,8 +128,12 @@ void updateThrottleOut(int &pulse, int &drive_state){
         
       setRpm2 = 0;                                // Second needs to be Low to drive Forwards
       analogWrite(PWMout2, setRpm2);              // Set second PWM Low
-      delay(10);                                  // Small delay
       analogWrite(PWMout1, setRpm1);              // Set first PWM to PWM signal
+
+      Serial.print("1: ");
+      Serial.println(setRpm1);
+      Serial.print("2: ");
+      Serial.println(setRpm2);
     
   } else if(drive_state == 2){                    // Reverse - Output Low High to drive backwards (just opposite of drive)
       if(pulse > 1100 && pulse < 2050)
@@ -159,6 +154,11 @@ void updateThrottleOut(int &pulse, int &drive_state){
   }
 }
 
+/* takes in 1 int named pulse
+ * pulse is the pulse width in us from pin 4, or 5 on the reciever
+ * based on the pulse width from input, turns on or off the headlights on the car
+ * return void
+ */
 void updateHeadlights(int &pulse){                // Reads in if pulse is long or short, and turns on and off headlights
     if(pulse > 1500 && pulse < 2050)
       digitalWrite(lightsPIN, HIGH);              // Turn on headlights
@@ -166,6 +166,13 @@ void updateHeadlights(int &pulse){                // Reads in if pulse is long o
       digitalWrite(lightsPIN, LOW);               // Turn off headlights    
 }
 
+/*  takes in one int named pulse
+ *  pulse is the pulse width in us from pin 5, 6 on reciever
+ *  returns 0, 1, or 2 for drive state
+ *  0 = neutral
+ *  1 = drive
+ *  2 = reverse
+ */
 int updateDriveState(int &pulse){
   if(pulse > 1600 && pulse < 2050){               // Reverse
     return 2;
@@ -173,22 +180,37 @@ int updateDriveState(int &pulse){
       return 1;
   } else if(pulse > 950 && pulse <= 1400){        // Neutral
       return 0;
+  } else {                                        // Default to Neutral
+    return 0;
   }
 }
 
+/* Takes in one integer named pulse
+ *  pulse is the pulse width in us from pin 3 on mega, or 4 on reciever
+ *  maps this pulse width to steer the car in the corresponding direction
+ *  no return value
+ */
 void updateSteering(int &pulse){
+  static int servoPosNew = 90;
+  
   if(pulse < 950 || pulse > 2050){                // If invalid pulse length, return to original position 
     steeringServo.write(90);  
     return;
   } 
-  servoPosNew = map(pulse, 980, 2020, 0, 180);    // + 12. Will need to change this to only rotate 90 degrees with of offset of 12 currently in order to recenter after turning 
-  if(servoPosNew > 180)
-    servoPosNew = 180;
+  
+  servoPosNew = map(pulse, 950, 2050, 0, 180);
+  if(servoPosNew > 180) servoPosNew = 180;                    // don't allow it to go over 180 degrees
+  if(servoPosNew < 0)   servoPosNew = 0;                      // don't allow it to go below 0 degrees
+  if(servoPosNew < 100 && servoPosNew > 80) servoPosNew = 90; // stop the jitter when it turns on
     
   steeringServo.write(servoPosNew);               // Write the new position
   delay(50);
 }
 
+/*  takes in to arguments
+ *  returns an int for the closest object to both lidar sensors
+ *  return value is cm
+ */
 int distanceCheck(){
   static uint16_t distance_one = 0; 
   static uint16_t distance_two = 0;               // Initialize the Variables
